28a94f79a4244a3bc246b6d13c78d366eaaf97e0
Mikko Tukiainen (Silo.AI) <ext-mikko.tukiainen@awake.ai>
2020-04-16 09:50:12 +0300

    Made into pip installable

---
+++ ImageDataAugmentor/__init__.py
@@ -0,0 +1,6 @@
+from .dataframe_iterator import DataFrameIterator
+from .directory_iterator import DirectoryIterator
+from .image_data_augmentor import ImageDataAugmentor
+from .iterator import Iterator
+from .numpy_array_iterator import NumpyArrayIterator
+from .utils import *
